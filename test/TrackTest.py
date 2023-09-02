d8c34c7d4263f15b1574c7c6687aca45b0839387
01Dri <drikill01@gmail.com>
2023-09-01 21:01:51 -0300

    Primeiros testes unitarios

--- test/TrackTest.py
+++ test/TrackTest.py
@@ -5,10 +5,11 @@ from app.entities.Track import Track
 class TrackTests(unittest.TestCase):
 
     def test_track_info(self):
-        track = Track("diego", 266)
+        track = Track("diego", 266, "url_musica")
         self.assertIsNotNone(track)
         self.assertEqual("diego", track.name)
         self.assertEqual(266, track.time)
+        self.assertEqual("url_musica", track.url)
 
 
 if __name__ == '__main__':
